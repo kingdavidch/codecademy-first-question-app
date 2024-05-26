565d1e430f9b3e70847c7ea8d8c6900f272ff4d9
KINGDAVID CHUKWUMERE <kingdavidchukwumere@KINGDAVIDs-Laptop.local>
2024-05-26 13:48:58 +0200

    1 king

---
+++ CD first question app/CD first question app/CD_first_question_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  CD_first_question_appApp.swift
+//  CD first question app
+//
+//  Created by KINGDAVID CHUKWUMERE on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CD_first_question_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
